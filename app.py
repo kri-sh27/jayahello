b3b1a3770ad40c93d12b44089d8f9ac5e6f2fe12
kri-sh27 <hogalekrishna2000@gmail.com>
2025-06-20 10:40:44 +0530

    updated

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def serve_index():
     return send_from_directory('.', 'index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="54.163.208.162", port=5000)
 
